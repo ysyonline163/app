73a7bc593af4b793429c9aaff6361c25d0c3a38d
ysyonline <937026716@qq.com>
2016-12-02 00:53:27 +0800

    app.js modify with array execise

--- array.js
+++ array.js
@@ -1,15 +1,16 @@
 /**
  * Created by Administrator on 2016/11/28 0028.
  */
+'use strict';
 (function( o ){
-    //================================= indexOf ========================================
+    //================================= indexOf ==================================
     (function(){
         var arr = [1,2,3,4,5];
         var pos = arr.indexOf(4);
         console.log("indexOf ==>" + pos);
     })('indexOf');
 
-    //================================= slice ========================================
+    //================================= slice ====================================
     (function(){
         var arr = [1,2,3,4,5];
         var slice_arry = arr.slice( 2, 4 );
@@ -18,7 +19,7 @@
         console.log(slice_arry === arr );
     })('slice');
 
-    //================================ push & pop ===========================================
+    //================================ push & pop ================================
     (function(){
         var arr = [1,2,3,4,5];
         arr.push('a','b','c');
@@ -27,7 +28,7 @@
         console.log("pop==>"+arr);
     })('push & pop');
 
-    //================================ shift & unshift ===========================================
+    //================================ shift & unshift ===========================
     (function(){
         var arr = [1,2,3,4,5];
         arr.unshift('aaa');
@@ -36,13 +37,30 @@
         console.log('shift==>'+arr);
     })('shift & unshift');
 
+    //================================ splice =====================================
+    (function(){
+        var arr = [1,2,3,4,5];
+        arr.splice( 1, 3, 'a', 'b', 'c' );
+        console.log('splice==>'+arr);
+        arr.splice( 1, 0, 'a', 'b', 'c' );
+        console.log('splice==>' + arr);
+    })('splice');
 
+    //================================ concat =====================================
+    (function(){
+        var arr =[];
+        var brr = [1,2,3,4,5];
+        var concat_arr = arr.concat(brr);
+        console.log('concat==>' + concat_arr);
+    })('concat');
 
-
-
-
-
-
+    //================================ join =====================================
+    (function(){
+        var arr = ['2016','12','2'];
+        var brr = ['00','59','32'];
+        var time = arr.join('-') + " " + brr.join(':');
+        console.log(time);
+    })('join');
 
 
 
